0d628332bbf3fdd8a2b537c442c78652f81a2692
Hrithik5 <60300927+Hrithik5@users.noreply.github.com>
2021-04-01 22:27:35 +0530

    url path for home.html added

--- drf/api/urls.py
+++ drf/api/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.apiOverview, name='Overview'),
+    path('', views.home, name='Home'),
+    path('links/', views.apiOverview, name='Overview'),
     path('todo-list/', views.TodoList, name='TodoList'),
     path('todo-detail/<str:pk>/', views.TodoDetailView, name='TodoDetailView'),
     path('todo-create/', views.TodoCreate, name='TodoCreate'),
     path('todo-update/<str:pk>/', views.TodoUpdate, name='TodoUpdate'),
-    path('todo-delete/<str:pk>', views.TodoDelete, name='TodoDelete'),
+    path('todo-delete/<str:pk>/', views.TodoDelete, name='TodoDelete'),
 ]
